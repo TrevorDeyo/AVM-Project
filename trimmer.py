6ce019bf3763e8b9b2876af6dc7ccce482a6f59c
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-06-09 15:40:26 -0500

    trimming complete

--- trimmer.py
+++ trimmer.py
@@ -9,24 +9,23 @@ output = "C:\\Users\\tdeyo\\Desktop\\Code\\AVM-Project\\2trimmed_audio\\"
 file_count = sum(len(files) for _, _, files in os.walk(directory_path))
 
 counter = 0
-start_point = 73087
 
-with alive_bar(file_count - start_point) as bar:
+with alive_bar(file_count) as bar:
     for root, dirs, files in os.walk(directory_path):
         for file in files:
-            if counter > start_point:
-                file_path = os.path.join(root, file)
-                try:    
-                    original_sound = AudioSegment.from_file(file_path)
-                    audio_segments = silence.split_on_silence(original_sound,
-                                                            min_silence_len = 2000,
-                                                            silence_thresh = -45,
-                                                            keep_silence = 500,)
-                    processed_sound = sum(audio_segments)
 
-                    processed_sound.export(f"{output}{counter}.wav", format="wav")
-                    bar()
-                except:
-                    print(f"failed processing: {file_path} | Step Number: {counter}")
+            file_path = os.path.join(root, file)
+            try:    
+                original_sound = AudioSegment.from_file(file_path)
+                audio_segments = silence.split_on_silence(original_sound,
+                                                        min_silence_len = 2000,
+                                                        silence_thresh = -45,
+                                                        keep_silence = 500,)
+                processed_sound = sum(audio_segments)
+
+                processed_sound.export(f"{output}{counter}.wav", format="wav")
+                bar()
+            except:
+                print(f"failed processing: {file_path} | Step Number: {counter}")
 
             counter += 1
